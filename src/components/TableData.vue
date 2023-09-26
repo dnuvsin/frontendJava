78dae41c7f93744c40925993eec663767bfffcb4
dnuvsin <92454497+dnuvsin@users.noreply.github.com>
2023-09-26 21:51:37 +0700

    lab 11 09/20

--- src/components/TableData.vue
+++ src/components/TableData.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <v-data-table :headers="headers" :items="employeeItem" class="elevation-4 mt-2">
+    <v-data-table :headers="headers" :items="employeeItem" class="elevation-1">
         <template v-slot:top>
             <v-toolbar flat>
                 <v-toolbar-title>จัดการข้อมูล</v-toolbar-title>
@@ -12,22 +12,64 @@
             </v-toolbar>
         </template>
         <template v-slot:[`item.role`]="{ item }">
-          {{ item.role.name }}
+            {{ item.role.name }}
         </template>
         <template v-slot:[`item.actions`]="{ item }">
-            <v-btn small outlined @click="openDialog('edit', item)" color="blue">
-                <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-            <v-btn small outlined @click="deleteItem(item)" color="red" class="ml-2">
-                <v-icon>mdi-delete</v-icon>
+            <v-icon small outline class="mr-2" @click="openDialog('edit', item)" color="blue">
+                mdi-pencil
+            </v-icon>
+            <v-icon small outline @click="deleteItem(item)" color="red" class="m1-2">
+                mdi-delete
+            </v-icon>
+        </template>
+        <template v-slot:no-data>
+            <v-btn color="primary" @click="initialize">
+                Reset
             </v-btn>
         </template>
     </v-data-table>
-    <v-dialog v-model="dialog" max-width="500px">
+    <v-dialog v-model="dialogCreate" max-width="500px">
         <v-card>
             <v-card-title>
                 <span class="text-h5">{{ formTitle }}</span>
             </v-card-title>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="6">
+                            <v-text-field v-model="employeeId" label="ไอดี"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="6">
+                            <v-text-field v-model="firstName" label="ชื่อ"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="6">
+                            <v-text-field v-model="lastName" label="นามสกุล"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="6">
+                            <v-text-field v-model="salary" label="เงินเดือน"></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save(formTitle)">
+                    บันทึกข้อมูล
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialogEdit" max-width="500px">
+        <v-card>
+            <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
             <v-card-text>
                 <v-container>
                     <v-row>
@@ -40,7 +82,7 @@
                         <v-col cols="12" sm="6" md="6">
                             <v-text-field v-model="salary" label="เงินเดือน"></v-text-field>
                         </v-col>
-                        <v-col cols="12" sm="6" md="6">
+                      <v-col cols="12" sm="6" md="6">
                             <v-text-field v-model="role" label="ตำแหน่ง"></v-text-field>
                         </v-col>
                     </v-row>
@@ -48,62 +90,82 @@
             </v-card-text>
 
             <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="close">
-                ยกเลิก
-              </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="save(formTitle)">
-                บันทึกข้อมูล
-              </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save(formTitle)">
+                    บันทึกข้อมูล
+                </v-btn>
             </v-card-actions>
         </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-  </v-data-table>
+    </v-dialog>
+    <v-dialog v-model="dialogDelete" max-width="500px">
+        <v-card>
+            <v-card-title class="text-h5">คุณต้องการลบข้อมูลนี้ในตารางหรือไม่?</v-card-title>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete()">ยกเลิก</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm()">ตกลง</v-btn>
+                <v-spacer></v-spacer>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</div>
 </template>
 
 <script>
+
 export default {
-  data () {
-    return {
-      dialog: false,
-      formTitle: '',
-      editedIndex: -1,
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      },
-      defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      },
-      headers: [
-        {
-          text: 'ชื่อของหวาน',
-          align: 'start',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'แคลอรี่', value: 'calories' },
-        { text: 'ไขมัน', value: 'fat' },
-        { text: 'คาร์โบไฮเดรต', value: 'carbs' },
-        { text: 'โปรตีน', value: 'protein' },
-        { text: 'จัดการ', value: 'actions', sortable: false }
-      ],
-      desserts: []
+  data: () => ({
+    dialogCreate: false,
+    dialogEdit: false,
+    dialogDelete: false,
+    employeeId: '',
+    firstName: '',
+    lastName: '',
+    role: '',
+    salary: '',
+    headers: [{
+      text: 'ไอดี',
+      align: 'start',
+      sortable: false,
+      value: 'employeeId'
+    },
+    {
+      text: 'ชื่อ',
+      value: 'firstName'
+    },
+    {
+      text: 'นามสกุล',
+      value: 'lastName'
+    },
+    {
+      text: 'ตำแหน่ง',
+      value: 'role'
+    },
+    {
+      text: 'เงินเดือน',
+      value: 'salary'
+    },
+    {
+      text: 'Actions',
+      value: 'actions',
+      sortable: false
+    }
+    ],
+    employeeItem: [],
+    formTitle: '',
+    idEmployee: '',
+    idForDelete: ''
+  }),
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
     }
   },
 
@@ -116,92 +178,97 @@ export default {
       this.employeeItem = []
       try {
         const data = await this.axios.get('http://localhost:9000/employee')
-        console.log('data employee ====', data)
+        console.log('data employee ====>', data)
         this.employeeItem = data.data
-      } catch (error) {
-      }
+      } catch (error) {}
     },
+
     openDialog (Action, item) {
       this.formTitle = ''
       if (Action === 'add') {
+        this.dialogCreate = true
         this.formTitle = 'เพิ่มข้อมูล'
         this.editedItem = item
-        this.dialog = true
       } else {
         this.formTitle = 'แก้ไขข้อมูล'
-        this.dialog = true
+        this.dialogEdit = true
         this.firstName = item.firstName
         this.lastName = item.lastName
         this.salary = item.salary
         this.role = item.role.name
-        this.idEmployee = item.id
+        this.idEmployee = item.employeeId
       }
     },
-    close () {
-      this.dialog = false
-      this.firstName = ''
-      this.lastName = ''
-      this.salary = ''
-      this.role = ''
+
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
     },
-    async save (action) {
-      if (action === 'เพิ่มข้อมูล') {
-        // this.desserts.push(this.editedItem)
-        // this.close()
-        // eslint-disable-next-line
-        var data = {
-          firstName: this.firstName,
-          lastName: this.lastName,
-          salary: this.salary,
-          role: { name: this.role },
-          skills: [
-            { skill: '' }
-          ]
-        }
-        console.log('data after send ===>', data)
-        try {
-          // eslint-disable-next-line
-          var dataResponse = await this.axios.post('http://localhost:9000/employee', data)
-          console.log('dataResponse ===> ', dataResponse)
-          this.close()
-          this.initialize()
-        } catch (error) {
-          console.log(error.message)
-        }
-      } else {
-        try {
-          // eslint-disable-next-line
-          var dataResponseEdit = await this.axios.put('http://localhost:9000/employee/' + this.idForDelete, data)
-          this.close()
-          this.initialize()
-        } catch (error) {
-          console.log(error.message)
-        }
-      }
-      // this.close()
-    },
+
     deleteItem (item) {
       this.idForDelete = item.id
       this.dialogDelete = true
     },
+
     async deleteItemConfirm () {
       try {
-        // eslint-disable-next-line
-        var response = await this.axios.delete('http://localhost:9000/employee/' + this.idForDelete, data)
+        const response = await this.axios.delete(`http://localhost:9000/employee/${this.idForDelete}`)
         this.initialize()
       } catch (error) {
         console.log(error.message)
       }
       this.closeDelete()
     },
-    closeDelete () {
-      this.dialogDelete = false
+
+    close () {
+      this.dialogCreate = false
+      this.dialogEdit = false
+      this.employeeId = ''
       this.firstName = ''
       this.lastName = ''
       this.salary = ''
       this.role = ''
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.editedItem = []
+      this.editedIndex = -1
+    },
+
+    async save (action) {
+      const data = {
+        employeeId: this.employeeId,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        salary: this.salary,
+        role: { name: this.role },
+        skills: [
+          { skill: '' }
+        ]
+      }
+      if (action === 'เพิ่มข้อมูล') {
+        try {
+          const dataResponse = await this.axios.post('http://localhost:9000/employee', data)
+          // console.log('dataResponse ', dataResponse)
+          this.close()
+          this.initialize()
+        } catch (error) {
+          console.log(error.message)
+        }
+      } else {
+        try {
+          const dataResponseEdit = await this.axios.put(`http://localhost:9000/employee/${this.idEmployee}`, data)
+          // console.log('dataResponse ', dataResponseEdit)
+          this.close()
+          this.initialize()
+        } catch (error) {
+          console.log(error.message)
+        }
+      }
+      this.close()
     }
   }
-
 }
 </script>
